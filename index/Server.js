9b12abcc0e53fdb8ccbd8591797eca70096d9a40
Wise1944 <wise1944@yandex.ru>
2020-02-06 18:59:10 +0300

    jinnee
    final_1

--- index/Server.js
+++ index/Server.js
@@ -1,18 +1,21 @@
 var mysql = require('mysql');
-var database = mysql.createConnection({
-  host: 'host',
-  user: 'user',
-  password: 'password',
-  database: 'database'
-});
-database.connect(function(connectionError){
-  if(connectionError){
-    throw connectionError;
-  }
-  var sql = "INSERT INTO table(column) VALUES ('" + jsonData + "')";
-  database.query(sql, function(queryError, queryResult){
-    if(queryError){
-      throw queryError;
-    }
-  });
-});
+   var database = mysql.createConnection({
+     host: 'host',
+     user: 'user',
+     password: 'password',
+     database: 'database'
+   });
+   database.connect(function(connectionError){
+     if(connectionError){
+       throw connectionError;
+       console.log("Connected!");
+       var mysql="CREATE TABLE customers(name VARCHAR(255)),password VARCHAR(255)"
+     }
+
+     database.query(sql, function(queryError, queryResult){
+       if(queryError){
+         throw queryError;
+         console.log("create table")
+       }
+     });
+   });
